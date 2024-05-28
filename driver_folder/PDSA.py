f6eb053e99203241e28ed50e115213c272c247e9
pk1308 <princevkurien@gmail.com>
2024-05-28 11:20:08 +0400

    kruskal 5.6

--- driver_folder/PDSA.py
+++ driver_folder/PDSA.py
@@ -6,6 +6,24 @@ from shared.functions import get_git_status_files, create_md, update_my_docs, de
 
 logger.remove()
 logger.add(sys.stdout, colorize=True, format="{time} | {level} | {message}")
+def git_add_and_commit(commit_message):
+    """
+    Adds all changes to the staging area and commits them with the given commit message.
+    
+    Args:
+    commit_message (str): The commit message to use for the commit.
+    """
+    try:
+        # Add all changes to the staging area
+        subprocess.run(["git", "add", "."], check=True)
+        logger.info("Changes added to staging area.")
+
+        # Commit the changes with the provided commit message
+        subprocess.run(["git", "commit", "-m", commit_message], check=True)
+        logger.info(f"Changes committed with message: {commit_message}")
+
+    except subprocess.CalledProcessError as e:
+        logger.info(f"An error occurred: {e}")
 
 if __name__ == "__main__":
 
@@ -27,3 +45,5 @@ if __name__ == "__main__":
 
     deploy_mkdocs()
     logger.info("deployed mk docs ")
+    commit_message = input("please enter the commit message : ")
+    git_add_and_commit(commit_message)
